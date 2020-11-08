e127c87c04125ed846bf75abbf3253c6008de2d9
Fernandorodriguesb <fernando.nascimento@aluno.faculdadeimpacta.com.br>
2020-11-08 20:50:09 -0300

    ac4

--- t8.py
+++ t8.py
@@ -7,10 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def nao_entre_em_panico():
 
-    primos = "tudo vai dar certo caros alunos!"
+
+    primos = "Tudo vai dar certo, caros alunos!"
+
 
     return primos
 
 if __name__ == "__main__":
-    port = int(os.environ.get ("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
